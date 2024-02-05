92948ed4f686b6d4cabb22ff768b27b89905bf73
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-02-04 21:22:01 -0300

    card input terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
+import { Container } from "react-bootstrap";
+import "./App.css";
+import { SelectColor } from "./components/SelectColor";
 
 function App() {
   return (
     <>
-      
+      <main className="py-5">
+        <Container fluid className="d-flex justify-content-center p-0 p-md-2">
+          <SelectColor />
+        </Container>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/SelectColor.jsx
+++ src/components/SelectColor.jsx
@@ -1,6 +1,34 @@
+import { Button, FormControl } from "react-bootstrap";
 
 export const SelectColor = () => {
   return (
-    <div>SelectColor</div>
-  )
-}
+    <div className="shadow-sm w-75">
+      <div className="p-3 shadow-sm">
+        <h5>Administrar colores</h5>
+      </div>
+
+      <div>
+        <form>
+
+          <section className="inputColorContainer d-flex flex-column flex-md-row align-items-center py-4 px-md-5">
+            <div className="inputColorDiv mb-3 me-md-5">
+              <input
+                className=""
+                type="color"
+                name="selectColor"
+                id="selectColor"
+              />
+            </div>
+            <div className="inputTextColor">
+              <FormControl className="py-2" placeholder="Ingrese un color ej Blue" />
+            </div>
+          </section>
+
+          <section className="p-4 d-flex justify-content-end shadow-sm">
+            <Button>Guardar</Button>
+          </section>
+        </form>
+      </div>
+    </div>
+  );
+};
